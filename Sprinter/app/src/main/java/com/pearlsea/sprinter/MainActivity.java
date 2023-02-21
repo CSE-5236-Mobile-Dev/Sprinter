29736921dedc9c32c2b998be33780ce86e9b3519
SavageGarrett <carder.320@buckeyemail.osu.edu>
2023-02-21 14:47:05 -0500

    Running Activity Note: you may need to rebuild or sync gradle files to successfully run

--- Sprinter/app/src/main/java/com/pearlsea/sprinter/MainActivity.java
+++ Sprinter/app/src/main/java/com/pearlsea/sprinter/MainActivity.java
@@ -1,14 +1,18 @@
 package com.pearlsea.sprinter;
 
 import androidx.appcompat.app.AppCompatActivity;
+import com.pearlsea.sprinter.databinding.ActivityMainBinding;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 }
