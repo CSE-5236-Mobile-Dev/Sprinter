932e1a6f0520789e4c7145c132e153cc222b80cf
selemonbushra <bushra.3@buckeyemail.osu.edu>
2023-03-08 17:21:25 -0500

    Login Fragment update

--- Sprinter/app/src/main/java/com/pearlsea/sprinter/LoginFragment.java
+++ Sprinter/app/src/main/java/com/pearlsea/sprinter/LoginFragment.java
@@ -1,76 +1,115 @@
 package com.pearlsea.sprinter;
-import static android.app.PendingIntent.getActivity;
-import static androidx.databinding.DataBindingUtil.setContentView;
 
 import android.os.Bundle;
-
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.content.Context;
+import android.widget.Button;
+import android.widget.Toast;
+import com.pearlsea.sprinter.db.User;
 
-import android.content.Intent;
-import android.widget.FrameLayout;
-
-import com.google.android.material.textfield.TextInputEditText;
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link LoginFragment} factory method to
+ * Activities that contain this fragment must implement the
+ * {@link LoginFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link LoginFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
+public class LoginFragment extends Fragment {
 
-// TODO: Rename and change types and number of parameters
-public class LoginFragment extends MainActivity {
+    EditText emailEditText;
+    EditText passEditText;
+    Button loginButton;
+    Button signupButton;
+
+    private OnFragmentInteractionListener listener;
 
     public LoginFragment() {
         // Required empty public constructor
     }
 
+    // TODO: Rename and change types and number of parameters
+    public static LoginFragment newInstance() {
+        return new LoginFragment();
+    }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_login);
-
     }
 
-    public void setContentView(int fragment_login) {
-        Log.d("SignupFragment", "onCreateView Lifecycle Method Triggered");
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
 
-        View rootView = inflater.inflate(R.layout.fragment_signup, container, false);
+        View view = inflater.inflate(R.layout.fragment_login, container, false);
+        emailEditText = view.findViewById(R.id.emailAddressTextBox);
+        passEditText = view.findViewById(R.id.passwordTextBox);
+        loginButton = view.findViewById(R.id.loginButton);
+        //signupButton = view.findViewById(R.id.signup_button);
 
-        FrameLayout signupScreenBackButton = rootView.findViewById(R.id.signupBackToWelcome);
-
-        View loginScreenBackButton = null;
-        loginScreenBackButton.setOnClickListener(new View.OnClickListener(){
+        loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                MainActivity mainActivity = (MainActivity) getActivity();
-                mainActivity.transitionToWelcome();
+                User user = new User();
+                String email = emailEditText.getText().toString().trim();
+                String pass = passEditText.getText().toString().trim();
+                if (!email.equals("") && !pass.equals("")) {
+                    user.setEmail(email);
+                    user.setPassword(pass);
+                    listener.onLoginClicked(user);
+                } else {
+                    Toast.makeText(getContext(), "invalid email or password", Toast.LENGTH_LONG).show();
+                }
             }
         });
 
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                listener.onNavigationToSignupClicked();
+            }
+        });
+        return view;
     }
 
-    public void LoginHandler(View target) {
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            listener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
 
-        Intent myintent = new Intent(this, MainActivity.class);
-        Bundle bundle = new Bundle();
-        bundle.putString("key1", "GFG :- Main Activity");
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        listener = null;
+    }
 
-////        pass user input
-        TextInputEditText username = (TextInputEditText) findViewById(R.id.email);
-        TextInputEditText password = (TextInputEditText) findViewById(R.id.password);
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+//        void onFragmentInteraction(Uri uri);
+        void onLoginClicked(User user);
 
-        String usernameText = email.getText().toString();
-        String passwordText = password.getText().toString();
-
-        bundle.putString("username", usernameText);
-        bundle.putString("password", passwordText);
-
-        myintent.putExtras(bundle);
-        startActivity(myintent);
-
+        void onNavigationToSignupClicked();
     }
 }
