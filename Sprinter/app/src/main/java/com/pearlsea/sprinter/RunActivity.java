16cde5ce13243c1c295ae1c5958c35331a13ea12
SavageGarrett <carder.320@osu.edu>
2023-03-29 14:10:52 -0400

    Location Updates Every 5 Seconds

--- Sprinter/app/src/main/java/com/pearlsea/sprinter/RunActivity.java
+++ Sprinter/app/src/main/java/com/pearlsea/sprinter/RunActivity.java
@@ -1,17 +1,34 @@
 package com.pearlsea.sprinter;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Looper;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.pearlsea.sprinter.db.SprinterDatabase;
 
 public class RunActivity extends AppCompatActivity implements View.OnClickListener {
@@ -26,6 +43,13 @@ public class RunActivity extends AppCompatActivity implements View.OnClickListen
 
     /* Declare Fragments */
     SettingsFragment settingsFragment;
+
+    // initializing
+    // FusedLocationProviderClient
+    // object
+    FusedLocationProviderClient mFusedLocationClient;
+    public double latitude, longitude;
+    int PERMISSION_ID = 44;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,6 +71,11 @@ public class RunActivity extends AppCompatActivity implements View.OnClickListen
         /* Add a Listener to the Settings Button */
         settingsButton = findViewById(R.id.settings_button);
         settingsButton.setOnClickListener(this);
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        // method to get the location
+        getLastLocation();
     }
 
     public void onClick(View v) {
@@ -89,6 +118,117 @@ public class RunActivity extends AppCompatActivity implements View.OnClickListen
     public void transitionToWelcomeScreen() {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+    }
+    //endregion
+
+    //region Location Permissions / Request
+    @SuppressLint("MissingPermission")
+    public void getLastLocation() {
+        // check if permissions are given
+        if (checkPermissions()) {
+
+            // check if location is enabled
+            if (isLocationEnabled()) {
+
+                // getting last
+                // location from
+                // FusedLocationClient
+                // object
+                mFusedLocationClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Location> task) {
+                        Location location = task.getResult();
+                        if (location == null) {
+                            requestNewLocationData();
+                        } else {
+                            latitude = location.getLatitude();
+                            longitude = location.getLongitude();
+                            Log.d("RunActivity", latitude + " " + longitude);
+                        }
+                    }
+                });
+            } else {
+                Toast.makeText(this, "Please turn on" + " your location...", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(intent);
+            }
+        } else {
+            // if permissions aren't available,
+            // request for permissions
+            requestPermissions();
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private void requestNewLocationData() {
+
+        // Initializing LocationRequest
+        // object with appropriate methods
+        LocationRequest mLocationRequest = new LocationRequest();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(5000);
+        mLocationRequest.setFastestInterval(0);
+
+        // setting LocationRequest
+        // on FusedLocationClient
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        mFusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper());
+    }
+
+    private LocationCallback mLocationCallback = new LocationCallback() {
+
+        @Override
+        public void onLocationResult(LocationResult locationResult) {
+            Location mLastLocation = locationResult.getLastLocation();
+            latitude = mLastLocation.getLatitude();
+            longitude = mLastLocation.getLongitude();
+            Log.d("RunActivity", latitude + " " + longitude);
+        }
+    };
+
+    // method to check for permissions
+    private boolean checkPermissions() {
+        return ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+
+        // If we want background location
+        // on Android 10.0 and higher,
+        // use:
+        // ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_BACKGROUND_LOCATION) == PackageManager.PERMISSION_GRANTED
+    }
+
+    // method to request for permissions
+    private void requestPermissions() {
+        ActivityCompat.requestPermissions(this, new String[]{
+                android.Manifest.permission.ACCESS_COARSE_LOCATION,
+                android.Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_ID);
+    }
+
+    // method to check
+    // if location is enabled
+    private boolean isLocationEnabled() {
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+    }
+
+    // If everything is alright then
+    @Override
+    public void
+    onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == PERMISSION_ID) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getLastLocation();
+            }
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (checkPermissions()) {
+            getLastLocation();
+        }
     }
     //endregion
 }
--- Sprinter/app/src/main/java/com/pearlsea/sprinter/RunningFragment.java
+++ Sprinter/app/src/main/java/com/pearlsea/sprinter/RunningFragment.java
@@ -1,8 +1,12 @@
 package com.pearlsea.sprinter;
 
+import static androidx.core.content.ContextCompat.getSystemService;
+
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -10,6 +14,8 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.os.Handler;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -43,7 +49,6 @@ public class RunningFragment extends Fragment implements OnMapReadyCallback {
      */
     private boolean permissionDenied = false;
 
-
     public RunningFragment() {
         // Required empty public constructor
     }
@@ -65,21 +70,84 @@ public class RunningFragment extends Fragment implements OnMapReadyCallback {
         return rootView;
     }
 
+    boolean mapReady = false;
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        mapReady = true;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions()
-                .position(sydney)
-                .title("Marker in Sydney"));
-        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 18));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+//        LatLng sydney = new LatLng(-34, 151);
+//        mMap.addMarker(new MarkerOptions()
+//                .position(sydney)
+//                .title("Marker in Sydney"));
+//        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+//        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 18));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        startLocationGathering();
     }
 
-    //region User Location Permissions
+    //region Handle Map Updates
+    private RunActivity parentActivity;
+    private Handler handler = new Handler();
 
+    /* Grab Location Data Every 5 Seconds */
+    private Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            // Set the Parent Activity
+            parentActivity = (RunActivity) getActivity();
+
+            if (parentActivity != null) {
+                parentActivity.getLastLocation();
+                double latitude = parentActivity.latitude;
+                double longitude = parentActivity.longitude;
+
+                Log.d("RunningFragment", latitude + " " + longitude);
+            }
+
+            handler.postDelayed(this, 5000); // Call this runnable again after 5 seconds
+        }
+    };
+
+    private boolean isRunning = false; // Flag to track the state of the location gathering
+
+    /* Resume Fragment - Resume Data Collection */
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (isRunning) {
+            handler.postDelayed(runnable, 5000); // Call the runnable for the first time
+        }
+    }
+
+    /* Stop Location Gathering when the Fragment is Paused */
+    @Override
+    public void onPause() {
+        super.onPause();
+        stopLocationGathering(); // Stop the runnable when the fragment is paused or stopped
+    }
+
+    /* Stop Location Gathering when the Fragment is Stopped */
+    @Override
+    public void onStop() {
+        super.onStop();
+        stopLocationGathering();
+    }
+
+    /* Start Control for Gathering Run Data */
+    public void startLocationGathering() {
+        if (!isRunning) {
+            handler.postDelayed(runnable, 5000); // Call the runnable for the first time
+            isRunning = true;
+        }
+    }
+
+    /* Stop Control for Gathering Run Data */
+    public void stopLocationGathering() {
+        handler.removeCallbacks(runnable); // Stop the runnable
+        isRunning = false;
+    }
     //endregion
 }
